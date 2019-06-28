f76003b60af8b174efe8bd17c0494b427e687180
Ziad Essam <Ziad Essam>
2019-06-28 14:59:26 +0200

    website added

--- js/script.js
+++ js/script.js
@@ -42,9 +42,22 @@ $(document).ready(function(){
   });
 
 function gotodiscord() {
-    location.replace("https://discordapp.com/api/oauth2/authorize?client_id=593054079710920722&permissions=0&redirect_uri=http%3A%2F%2Flocalhost%2FRaidAM%2Fdashboard.html&response_type=code&scope=identify%20guilds%20bot")
+    location.replace("https://discordapp.com/api/oauth2/authorize?client_id=593054079710920722&redirect_uri=http%3A%2F%2Flocalhost%2FRAM%2Fdashboard.html&response_type=token&scope=identify%20guilds")
 
 }
-
-
+function dropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
  
